6872a40cb597d8cf20a03fbc8b182ca175ac17d6
Luiza Pessoa <lluizapesssoa@gmail.com>
2024-12-01 12:08:56 -0300

    commit 2

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('core/', include('core.urls'))
+    path('', include('core.urls'))
 ]
